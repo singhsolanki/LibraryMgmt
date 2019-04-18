5752c2ccb3b8967f2e97e0d44e93783bd1cb285d
Sanjay <singh.solanki80@gmail.com>
2019-04-18 11:46:05 +0530

    New changes added

--- src/main/java/com/yash/dao/UserDAOImpl.java
+++ src/main/java/com/yash/dao/UserDAOImpl.java
@@ -4,9 +4,9 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
-import javax.sql.DataSource;
+//import javax.sql.DataSource;
 
-import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
